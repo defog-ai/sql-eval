d1e7c49db9cdb050b65f2c6896c4f70b7370e69e
JP <wongjingping@gmail.com>
2023-11-07 09:56:15 +0000

    Multiple Prompts - allows multiple prompts and output files in a single run. this saves the model loading time especially when testing multiple prompts for hf and vllm runners - we ensure that the number of prompt and output files match early in main.py since it applies to all runners

--- eval/openai_runner.py
+++ eval/openai_runner.py
@@ -1,5 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import copy
+import os
 from eval.eval import compare_query_results
 import pandas as pd
 from psycopg2.extensions import QueryCanceledError
@@ -9,71 +10,21 @@ from utils.questions import prepare_questions_df
 
 
 def run_openai_eval(args):
-    print("preparing questions...")
-    # get questions
-    question_query_df = prepare_questions_df(args.questions_file, args.num_questions)
+    for prompt_file, output_file in zip(args.prompt_file, args.output_file):
+        print("preparing questions...")
+        # get questions
+        question_query_df = prepare_questions_df(
+            args.questions_file, args.num_questions
+        )
 
-    input_rows = question_query_df.to_dict("records")
-    output_rows = []
-    with ThreadPoolExecutor(args.parallel_threads) as executor:
-        # for each query in the csv, generate a query using the generator asynchronously
-        futures = []
-        for row in input_rows:
-            # get db creds for each row's db_name
-            db_name = row["db_name"]
-            db_creds = {
-                "host": "localhost",
-                "port": 5432,
-                "user": "postgres",
-                "password": "postgres",
-                "database": db_name,
-            }
-
-            qg = OpenAIQueryGenerator(
-                db_creds=copy.deepcopy(db_creds),
-                model=args.model,
-                prompt_file=args.prompt_file,
-                timeout=args.timeout_gen,
-                use_public_data=not args.use_private_data,
-                verbose=args.verbose,
-            )
-
-            generated_query_fut = executor.submit(
-                qg.generate_query, question=row["question"]
-            )
-            futures.append(generated_query_fut)
-
-        total_tried = 0
-        total_correct = 0
-        for f in (pbar := tqdm(as_completed(futures), total=len(futures))):
-            total_tried += 1
-            i = futures.index(f)
-            row = input_rows[i]
-            result_dict = f.result()
-            query_gen = result_dict["query"]
-            reason = result_dict["reason"]
-            err = result_dict["err"]
-            # save custom metrics
-            if "latency_seconds" in result_dict:
-                row["latency_seconds"] = result_dict["latency_seconds"]
-            if "tokens_used" in result_dict:
-                row["tokens_used"] = result_dict["tokens_used"]
-            row["generated_query"] = query_gen
-            row["reason"] = reason
-            row["error_msg"] = err
-            # save failures into relevant columns in the dataframe
-            if "GENERATION ERROR" in err:
-                row["error_query_gen"] = 1
-            elif "EXECUTION ERROR" in err:
-                row["error_db_exec"] = 1
-            elif "TIMEOUT" in err:
-                row["timeout"] = 1
-            else:
-                expected_query = row["query"]
+        input_rows = question_query_df.to_dict("records")
+        output_rows = []
+        with ThreadPoolExecutor(args.parallel_threads) as executor:
+            # for each query in the csv, generate a query using the generator asynchronously
+            futures = []
+            for row in input_rows:
+                # get db creds for each row's db_name
                 db_name = row["db_name"]
-                question = row["question"]
-                query_category = row["query_category"]
-                exact_match = correct = 0
                 db_creds = {
                     "host": "localhost",
                     "port": 5432,
@@ -81,39 +32,96 @@ def run_openai_eval(args):
                     "password": "postgres",
                     "database": db_name,
                 }
-                # try executing the queries and compare the results if they succeed
-                try:
-                    exact_match, correct = compare_query_results(
-                        query_gold=expected_query,
-                        query_gen=query_gen,
-                        db_name=db_name,
-                        db_creds=db_creds,
-                        timeout=args.timeout_exec,
-                        question=question,
-                        query_category=query_category,
-                    )
-                    row["exact_match"] = int(exact_match)
-                    row["correct"] = int(correct)
-                    row["error_msg"] = ""
-                    if correct:
-                        total_correct += 1
-                except QueryCanceledError as e:
-                    row["timeout"] = 1
-                    row["error_msg"] = f"QUERY EXECUTION TIMEOUT: {e}"
-                except Exception as e:
-                    row["error_db_exec"] = 1
-                    row["error_msg"] = f"QUERY EXECUTION ERROR: {e}"
-            output_rows.append(row)
-            pbar.set_description(
-                f"Correct so far: {total_correct}/{total_tried} ({100*total_correct/total_tried:.2f}%)"
-            )
-    output_df = pd.DataFrame(output_rows)
-    output_df = output_df.sort_values(by=["db_name", "query_category", "question"])
-    if "prompt" in output_df.columns:
-        del output_df["prompt"]
-    print(output_df.groupby("query_category")[["exact_match", "correct"]].mean())
-    output_df.to_csv(args.output_file, index=False, float_format="%.2f")
 
-    # get average rate of correct results
-    avg_subset = output_df["correct"].sum() / len(output_df)
-    print(f"Average correct rate: {avg_subset:.2f}")
+                qg = OpenAIQueryGenerator(
+                    db_creds=copy.deepcopy(db_creds),
+                    model=args.model,
+                    prompt_file=prompt_file,
+                    timeout=args.timeout_gen,
+                    use_public_data=not args.use_private_data,
+                    verbose=args.verbose,
+                )
+
+                generated_query_fut = executor.submit(
+                    qg.generate_query, question=row["question"]
+                )
+                futures.append(generated_query_fut)
+
+            total_tried = 0
+            total_correct = 0
+            for f in (pbar := tqdm(as_completed(futures), total=len(futures))):
+                total_tried += 1
+                i = futures.index(f)
+                row = input_rows[i]
+                result_dict = f.result()
+                query_gen = result_dict["query"]
+                reason = result_dict["reason"]
+                err = result_dict["err"]
+                # save custom metrics
+                if "latency_seconds" in result_dict:
+                    row["latency_seconds"] = result_dict["latency_seconds"]
+                if "tokens_used" in result_dict:
+                    row["tokens_used"] = result_dict["tokens_used"]
+                row["generated_query"] = query_gen
+                row["reason"] = reason
+                row["error_msg"] = err
+                # save failures into relevant columns in the dataframe
+                if "GENERATION ERROR" in err:
+                    row["error_query_gen"] = 1
+                elif "EXECUTION ERROR" in err:
+                    row["error_db_exec"] = 1
+                elif "TIMEOUT" in err:
+                    row["timeout"] = 1
+                else:
+                    expected_query = row["query"]
+                    db_name = row["db_name"]
+                    question = row["question"]
+                    query_category = row["query_category"]
+                    exact_match = correct = 0
+                    db_creds = {
+                        "host": "localhost",
+                        "port": 5432,
+                        "user": "postgres",
+                        "password": "postgres",
+                        "database": db_name,
+                    }
+                    # try executing the queries and compare the results if they succeed
+                    try:
+                        exact_match, correct = compare_query_results(
+                            query_gold=expected_query,
+                            query_gen=query_gen,
+                            db_name=db_name,
+                            db_creds=db_creds,
+                            timeout=args.timeout_exec,
+                            question=question,
+                            query_category=query_category,
+                        )
+                        row["exact_match"] = int(exact_match)
+                        row["correct"] = int(correct)
+                        row["error_msg"] = ""
+                        if correct:
+                            total_correct += 1
+                    except QueryCanceledError as e:
+                        row["timeout"] = 1
+                        row["error_msg"] = f"QUERY EXECUTION TIMEOUT: {e}"
+                    except Exception as e:
+                        row["error_db_exec"] = 1
+                        row["error_msg"] = f"QUERY EXECUTION ERROR: {e}"
+                output_rows.append(row)
+                pbar.set_description(
+                    f"Correct so far: {total_correct}/{total_tried} ({100*total_correct/total_tried:.2f}%)"
+                )
+        output_df = pd.DataFrame(output_rows)
+        output_df = output_df.sort_values(by=["db_name", "query_category", "question"])
+        if "prompt" in output_df.columns:
+            del output_df["prompt"]
+        print(output_df.groupby("query_category")[["exact_match", "correct"]].mean())
+        # get directory of output_file and create if not exist
+        output_dir = os.path.dirname(output_file)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_df.to_csv(output_file, index=False, float_format="%.2f")
+
+        # get average rate of correct results
+        avg_subset = output_df["correct"].sum() / len(output_df)
+        print(f"Average correct rate: {avg_subset:.2f}")
