90411b46a6363460fbfabebb3eb83ba6e958b608
JP <wongjingping@gmail.com>
2024-03-28 16:19:10 +0800

    Add basic instruct questions (#100)
    Add basic instruct questions
    Upgrade openai's runner and standard prompt generator to bypass pruning when num_columns = 0

--- utils/gen_prompt.py
+++ utils/gen_prompt.py
@@ -1,4 +1,4 @@
-from utils.pruning import prune_metadata_str
+from utils.pruning import prune_metadata_str, to_prompt_schema
 
 
 def generate_prompt(
@@ -19,25 +19,34 @@ def generate_prompt(
     columns_to_keep=40,
     shuffle_metadata=False,
 ):
+
+    from defog_data.metadata import dbs  # to avoid CI error
+
     with open(prompt_file, "r") as f:
         prompt = f.read()
     question_instructions = question + " " + instructions
 
     if table_metadata_string == "":
-        pruned_metadata_str = prune_metadata_str(
-            question_instructions,
-            db_name,
-            public_data,
-            columns_to_keep,
-            shuffle_metadata,
-        )
-    else:
-        pruned_metadata_str = table_metadata_string
+        if columns_to_keep > 0:
+            table_metadata_string = prune_metadata_str(
+                question_instructions,
+                db_name,
+                public_data,
+                columns_to_keep,
+                shuffle_metadata,
+            )
+        elif columns_to_keep == 0:
+            md = dbs[db_name]["table_metadata"]
+            table_metadata_string = to_prompt_schema(md, shuffle_metadata)
+        else:
+            raise ValueError("columns_to_keep must be >= 0")
+    if glossary == "":
+        glossary = dbs[db_name]["glossary"]
 
     prompt = prompt.format(
         user_question=question,
         instructions=instructions,
-        table_metadata_string=pruned_metadata_str,
+        table_metadata_string=table_metadata_string,
         k_shot_prompt=k_shot_prompt,
         glossary=glossary,
         prev_invalid_sql=prev_invalid_sql,
