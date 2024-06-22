da54167462164e4804eebb38f96cfeabe9671553
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-06-23 03:10:38 +0800

    added a google cloud function for uploading results to cloud storage

---
+++ utils/upload_report_gcloud.py
@@ -0,0 +1,19 @@
+# this is a Google cloud function for receiving the data from the web app and storing it in the database
+
+import functions_framework
+from google.cloud import storage
+import json
+
+BUCKET_NAME = "YOUR-BUCKET-NAME"
+
+
+@functions_framework.http
+def hello_http(request):
+    request_json = request.get_json(silent=True)
+    results = request_json["results"]
+    run_name = request_json["run_name"]
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(BUCKET_NAME)
+    blob = bucket.blob(run_name + ".json")
+    blob.upload_from_string(json.dumps(results))
+    return "success"
