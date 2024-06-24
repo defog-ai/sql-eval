3ba8d3974243efed9279a95b7f7b4e16d2e09318
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-06-25 03:53:55 +0800

    added initial slack message

--- analyze_results_and_post_to_slack.py
+++ analyze_results_and_post_to_slack.py
@@ -2,6 +2,8 @@ import os
 import argparse
 import pandas as pd
 from slack_sdk import WebClient
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -13,7 +15,7 @@ if __name__ == "__main__":
     for model_name in model_names:
         fnames = [i for i in os.listdir(f"results/{model_name}") if i.endswith(".csv")]
         for fname in fnames:
-            checkpoint = fname.split("_")[1]
+            checkpoint = fname.split(model_name)[1].split("_")[1]
             eval_type = fname.split("_")[-1]
             tdf = pd.read_csv(f"results/{model_name}/{fname}")
             tdf["checkpoint"] = checkpoint
@@ -25,10 +27,34 @@ if __name__ == "__main__":
 
     # first, get the average correct for each model, so that the index is the model name and each checkpoint is a column
     avg_correct = (
-        results.groupby(["model_name", "checkpoint"])["correct"].mean().unstack()
+        results[~results.query_category.isin(["cat_a", "cat_b", "cat_c"])]
+        .groupby(["model_name", "checkpoint"])["correct"]
+        .mean()
+        .unstack()
     )
     avg_correct = avg_correct.reset_index()
     avg_correct = avg_correct.melt(
         id_vars=["model_name"], var_name="checkpoint", value_name="avg_correct"
     )
     print(avg_correct)
+
+    # create a graph of the average correct for each model, with each model as a line and each checkpoint as a point on the x axis
+    plt.figure(figsize=(10, 10))
+    sns.relplot(
+        data=avg_correct,
+        x="checkpoint",
+        y="avg_correct",
+        col="model_name",
+    )
+    # save the graph
+    # this will get overwritten each time the script is run, but that's okay
+    sns.savefig("results/avg_correct.png")
+
+    # post the graph to slack
+    slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+    slack_client.files_upload_v2(
+        channels="#engineering",
+        title="Average Correct for each model",
+        file="results/avg_correct.png",
+        initial_comment="A set of evals just finished running! Here's the average correct rate for each model and each checkpoint that was in the evals.",
+    )
