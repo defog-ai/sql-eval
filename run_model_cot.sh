1b6161372d38656ab0d67450745256b5f94b107e
JP <wongjingping@gmail.com>
2024-07-19 04:58:18 +0000

    update run_model_cot.sh to use new prompts

--- run_model_cot.sh
+++ run_model_cot.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
-model_dir="${HOME}/models"
+# model_dir="${HOME}/models"
 # parse in model_names from args to script
 model_names=("$@")
+db_type="postgres"
 # if model_names is empty, print and exit
 if [ -z "$model_names" ]; then
   echo "No model names provided"
@@ -11,12 +12,19 @@ fi
 PORT=8084 # avoid 8081 as it's used by nginx
 export CUDA_VISIBLE_DEVICES=0 # set gpu you want to use (just 1 will do)
 
+# if db_type not postgres or sqlite, prompt_file should be prompts/prompt_cot.md else use prompts/prompt_cot_${dbtype}.md
+if [ "$db_type" != "postgres" ] && [ "$db_type" != "sqlite" ]; then
+  prompt_file="prompts/prompt_cot.md"
+else
+  prompt_file="prompts/prompt_cot_${db_type}.md"
+fi
+
 # Loop over model names
 for model_name in "${model_names[@]}"; do
 
   echo "Running model ${model_name}"
   # first, get the API up
-  python3 utils/api_server.py --model "${model_dir}/${model_name}" --tensor-parallel-size 1 --dtype float16 --max-model-len 8192 --gpu-memory-utilization 0.90 --block-size 16 --disable-log-requests --port "${PORT}" &
+  python3 utils/api_server.py --model "${model_name}" --tensor-parallel-size 1 --dtype float16 --max-model-len 8192 --gpu-memory-utilization 0.40 --block-size 16 --disable-log-requests --port "${PORT}" &
 
   # run a loop to check if the http://localhost:8080/health endpoint returns a valid 200 result
   while true; do
@@ -31,9 +39,9 @@ for model_name in "${model_names[@]}"; do
   done
 
   # then run sql-eval
-  python3 main.py -db postgres \
-    -f prompts/prompt_cot.md \
-    -q "data/questions_gen_postgres.csv" "data/instruct_basic_postgres.csv" "data/instruct_advanced_postgres.csv" "data/idk.csv" \
+  python3 main.py -db "${db_type}" \
+    -f "${prompt_file}" \
+    -q "data/questions_gen_${db_type}.csv" "data/instruct_basic_${db_type}.csv" "data/instruct_advanced_${db_type}.csv" "data/idk.csv" \
     -o "results/${model_name}/api_v1_cot.csv" "results/${model_name}/api_basic_cot.csv" "results/${model_name}/api_advanced_cot.csv" "results/${model_name}/api_idk_cot.csv" \
     -g api \
     -b 1 \
