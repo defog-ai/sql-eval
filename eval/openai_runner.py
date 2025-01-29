2b867865d09f435cc0003692d5dced754ab47817
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-01-30 01:49:42 +0800

    remove query generator from openai runner

--- eval/openai_runner.py
+++ eval/openai_runner.py
@@ -1,16 +1,136 @@
-import json
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import copy
 import os
+from time import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import json
+
+import pandas as pd
+import sqlparse
+from tqdm import tqdm
 
 from eval.eval import compare_query_results
-import pandas as pd
-from psycopg2.extensions import QueryCanceledError
-from query_generators.openai import OpenAIQueryGenerator
-from tqdm import tqdm
-from utils.questions import prepare_questions_df
 from utils.creds import db_creds_all
+from utils.dialects import convert_postgres_ddl_to_dialect
+from utils.gen_prompt import to_prompt_schema
+from utils.questions import prepare_questions_df
 from utils.reporting import upload_results
+from utils.llm import chat_openai
+
+
+def generate_prompt(
+    prompt_file,
+    question,
+    db_name,
+    db_type,
+    instructions="",
+    k_shot_prompt="",
+    glossary="",
+    table_metadata_string="",
+    prev_invalid_sql="",
+    prev_error_msg="",
+    public_data=True,
+    shuffle=True,
+):
+    if public_data:
+        from defog_data.metadata import dbs
+        import defog_data.supplementary as sup
+    else:
+        from defog_data_private.metadata import dbs
+        import defog_data_private.supplementary as sup
+
+    with open(prompt_file, "r") as f:
+        prompt = json.load(f)
+    
+    if table_metadata_string == "":
+        md = dbs[db_name]["table_metadata"]
+        pruned_metadata_ddl = to_prompt_schema(md, shuffle)
+        pruned_metadata_ddl = convert_postgres_ddl_to_dialect(
+            postgres_ddl=pruned_metadata_ddl,
+            to_dialect=db_type,
+            db_name=db_name,
+        )
+        column_join = sup.columns_join.get(db_name, {})
+        join_list = []
+        for values in column_join.values():
+            if isinstance(values[0], tuple):
+                for col_pair in values:
+                    col_1, col_2 = col_pair
+                    join_str = f"{col_1} can be joined with {col_2}"
+                    if join_str not in join_list:
+                        join_list.append(join_str)
+            else:
+                col_1, col_2 = values[0]
+                join_str = f"{col_1} can be joined with {col_2}"
+                if join_str not in join_list:
+                    join_list.append(join_str)
+        if len(join_list) > 0:
+            join_str = "\nHere is a list of joinable columns:\n" + "\n".join(join_list)
+        else:
+            join_str = ""
+        pruned_metadata_str = pruned_metadata_ddl + join_str
+    else:
+        pruned_metadata_str = table_metadata_string
+    
+    if prompt[0]["role"] == "system":
+        prompt[0]["content"] = prompt[0]["content"].format(
+            db_type=db_type,
+        )
+        prompt[1]["content"] = prompt[1]["content"].format(
+            user_question=question,
+            instructions=instructions,
+            table_metadata_string=pruned_metadata_str,
+            k_shot_prompt=k_shot_prompt,
+        )
+    else:
+        prompt[0]["content"] = prompt[1]["content"].format(
+            db_type=db_type,
+            user_question=question,
+            instructions=instructions,
+            table_metadata_string=pruned_metadata_str,
+            k_shot_prompt=k_shot_prompt,
+        )
+    return prompt
+
+
+def process_row(row, model_name, args):
+    start_time = time()
+    messages = generate_prompt(
+        prompt_file=args.prompt_file[0],
+        question=row["question"],
+        db_name=row["db_name"],
+        db_type=args.db_type,
+        instructions=row["instructions"],
+        k_shot_prompt=row["k_shot_prompt"],
+        glossary=row["glossary"],
+        table_metadata_string=row["table_metadata_string"],
+        prev_invalid_sql=row["prev_invalid_sql"],
+        prev_error_msg=row["prev_error_msg"],
+        public_data=not args.use_private_data,
+        shuffle=args.shuffle_metadata,
+    )
+    try:
+        response = chat_openai(messages=messages, model=model_name, temperature=0.0)
+        generated_query = response.content.split("```sql", 1)[-1].split("```", 1)[0].strip()
+        try:
+            generated_query = sqlparse.format(
+                generated_query, reindent=True, keyword_case="upper"
+            )
+        except:
+            pass
+        return {
+            "query": generated_query,
+            "reason": "",
+            "err": "",
+            "latency_seconds": time() - start_time,
+            "tokens_used": response.input_tokens + response.output_tokens,
+        }
+    except Exception as e:
+        return {
+            "query": "",
+            "reason": "",
+            "err": f"GENERATION ERROR: {str(e)}",
+            "latency_seconds": time() - start_time,
+            "tokens_used": 0,
+        }
 
 
 def run_openai_eval(args):
@@ -27,7 +147,6 @@ def run_openai_eval(args):
         questions_file_list, prompt_file_list, output_file_list
     ):
         print(f"Using prompt file {prompt_file}")
-        # get questions
         print("Preparing questions...")
         print(
             f"Using {'all' if num_questions is None else num_questions} question(s) from {questions_file}"
@@ -38,36 +157,13 @@ def run_openai_eval(args):
         input_rows = question_query_df.to_dict("records")
         output_rows = []
         with ThreadPoolExecutor(args.parallel_threads) as executor:
-            # for each query in the csv, generate a query using the generator asynchronously
             futures = []
             for row in input_rows:
-                # get db creds for each row's db_name
-                db_name = row["db_name"]
-                db_creds = db_creds_all[row["db_type"]]
-
-                qg = OpenAIQueryGenerator(
-                    db_creds=copy.deepcopy(db_creds),
-                    db_name=db_name,
-                    db_type=db_type,
-                    model=args.model,
-                    prompt_file=prompt_file,
-                    timeout=args.timeout_gen,
-                    use_public_data=not args.use_private_data,
-                    verbose=args.verbose,
-                )
-
                 generated_query_fut = executor.submit(
-                    qg.generate_query,
-                    question=row["question"],
-                    instructions=row["instructions"],
-                    k_shot_prompt=row["k_shot_prompt"],
-                    glossary=row["glossary"],
-                    table_metadata_string=row["table_metadata_string"],
-                    prev_invalid_sql=row["prev_invalid_sql"],
-                    prev_error_msg=row["prev_error_msg"],
-                    table_aliases=row["table_aliases"],
-                    columns_to_keep=args.num_columns,
-                    shuffle=args.shuffle_metadata,
+                    process_row,
+                    row=row,
+                    model_name=args.model,
+                    args=args,
                 )
                 futures.append(generated_query_fut)
 
@@ -81,7 +177,6 @@ def run_openai_eval(args):
                 query_gen = result_dict["query"]
                 reason = result_dict["reason"]
                 err = result_dict["err"]
-                table_metadata_string = result_dict["table_metadata_string"]
                 # save custom metrics
                 if "latency_seconds" in result_dict:
                     row["latency_seconds"] = result_dict["latency_seconds"]
@@ -90,52 +185,39 @@ def run_openai_eval(args):
                 row["generated_query"] = query_gen
                 row["reason"] = reason
                 row["error_msg"] = err
-                row["table_metadata_string"] = table_metadata_string
                 # save failures into relevant columns in the dataframe
                 if "GENERATION ERROR" in err:
                     row["error_query_gen"] = 1
-                elif "EXECUTION ERROR" in err:
-                    row["error_db_exec"] = 1
-                elif "TIMEOUT" in err:
-                    row["timeout"] = 1
                 else:
                     expected_query = row["query"]
                     db_name = row["db_name"]
                     db_type = row["db_type"]
-                    question = row["question"]
-                    query_category = row["query_category"]
-                    table_metadata_string = row["table_metadata_string"]
-                    exact_match = correct = 0
-                    db_creds = db_creds_all[db_type]
-                    # try executing the queries and compare the results if they succeed
                     try:
-                        exact_match, correct = compare_query_results(
+                        is_correct = compare_query_results(
                             query_gold=expected_query,
                             query_gen=query_gen,
                             db_name=db_name,
                             db_type=db_type,
-                            db_creds=db_creds,
-                            timeout=args.timeout_exec,
-                            question=question,
-                            query_category=query_category,
-                            table_metadata_string=table_metadata_string,
-                            decimal_points=args.decimal_points,
+                            question=row["question"],
+                            query_category=row["query_category"],
+                            db_creds=db_creds_all[db_type],
                         )
-                        row["exact_match"] = int(exact_match)
-                        row["correct"] = int(correct)
-                        row["error_msg"] = ""
-                        if correct:
+                        if is_correct:
                             total_correct += 1
-                    except QueryCanceledError as e:
-                        row["timeout"] = 1
-                        row["error_msg"] = f"QUERY EXECUTION TIMEOUT: {e}"
+                            row["is_correct"] = 1
+                            row["error_msg"] = ""
+                        else:
+                            row["is_correct"] = 0
+                            row["error_msg"] = "INCORRECT RESULTS"
                     except Exception as e:
                         row["error_db_exec"] = 1
-                        row["error_msg"] = f"QUERY EXECUTION ERROR: {e}"
+                        row["error_msg"] = f"EXECUTION ERROR: {str(e)}"
                 output_rows.append(row)
                 pbar.set_description(
-                    f"Correct so far: {total_correct}/{total_tried} ({100*total_correct/total_tried:.2f}%)"
+                    f"Accuracy: {round(total_correct/total_tried * 100, 2)}% ({total_correct}/{total_tried})"
                 )
+
+        # save results to csv
         output_df = pd.DataFrame(output_rows)
         output_df = output_df.sort_values(by=["db_name", "query_category", "question"])
         if "prompt" in output_df.columns:
@@ -145,7 +227,7 @@ def run_openai_eval(args):
             output_df.groupby("query_category")
             .agg(
                 num_rows=("db_name", "count"),
-                mean_correct=("correct", "mean"),
+                mean_correct=("is_correct", "mean"),
                 mean_error_db_exec=("error_db_exec", "mean"),
             )
             .reset_index()
